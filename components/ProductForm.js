0591d7dd00fe584c5b940aa8542fffafb5e7f486
Cam <cebusiness2022@gmail.com>
2023-05-08 15:49:23 -0600

    update

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -8,12 +8,13 @@ export default function ProductForm({
     title: existingTitle, 
     description: existingDescription, 
     price: existingPrice,
-    images
+    images: existingImages,
 }) {
 
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImages || []);
     const router = useRouter();
     const [goToProducts, setGoToProducts] = useState(false);
 
@@ -33,6 +34,20 @@ export default function ProductForm({
     if (goToProducts) {
         router.push('/products');
     }
+
+    async function uploadImages(ev){
+        const files = ev.target?.files
+        if (files?.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file );
+            }
+            const res = await axios.post('/api/upload', data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links];
+            });
+    };
+};
     return (
             <form onSubmit={saveProduct}>
                 <label>Product Name</label>
@@ -45,12 +60,19 @@ export default function ProductForm({
                     Images
                 </label>
                 <div className='mb-2'>
-                    <label className='w-24 h-24 text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-800 rounded-lg bg-gray-400'>
+                    {
+                        !!images?.length && images.map(link => (
+                            <div key={link}>
+                                <img src={link} alt=''/>
+                            </div>
+                        ))
+                    }
+                    <label className='w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-800 rounded-lg bg-gray-400'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                     </svg>
                         Upload
-                    <input type="file" className='hidden'/>
+                    <input type="file" onChange={uploadImages} className='hidden'/>
                     </label>
                     {
                         !images?.length && (
